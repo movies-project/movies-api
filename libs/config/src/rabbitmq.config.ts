79c5e5100afa0b9254cedfcc3534b85102375e20
AndreyKoshlan <aoaoaogit1010>
2023-04-25 13:13:05 +0300

    Переход с node:18-bullseye на node:18-alpine, устранена избыточность в названии переменных в rabbitmq.config.ts

--- libs/config/src/rabbitmq.config.ts
+++ libs/config/src/rabbitmq.config.ts
@@ -2,17 +2,17 @@ import { RmqOptions, Transport } from '@nestjs/microservices';
 import { ClientsModuleOptions } from "@nestjs/microservices/module/interfaces";
 import { SharedModule } from "@app/shared";
 
-const RABBITMQ_HOST = process.env.RABBITMQ_HOST || 'localhost';
-const RABBITMQ_PORT = process.env.RABBITMQ_PORT || 5672;
-const RABBITMQ_USER = process.env.RABBITMQ_USER || 'guest';
-const RABBITMQ_PASSWORD = process.env.RABBITMQ_PASSWORD || 'guest';
-const RABBITMQ_URL = `amqp://${RABBITMQ_USER}:${RABBITMQ_PASSWORD}@` +
-  `${RABBITMQ_HOST}:${RABBITMQ_PORT}`;
+const HOST = process.env.RABBITMQ_HOST || 'localhost';
+const PORT = process.env.RABBITMQ_PORT || 5672;
+const USER = process.env.RABBITMQ_USER || 'guest';
+const PASSWORD = process.env.RABBITMQ_PASSWORD || 'guest';
+const URL = `amqp://${USER}:${PASSWORD}@` +
+  `${HOST}:${PORT}`;
 
 const RMQ_OPTIONS = <RmqOptions>{
   transport: Transport.RMQ,
   options: {
-    urls: [RABBITMQ_URL],
+    urls: [URL],
     queueOptions: {
       durable: true,
     }

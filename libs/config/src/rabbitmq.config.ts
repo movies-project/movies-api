b954b76e4c4f801f286061d60ed43965b61dcd7a
AndreyKoshlan <aoaoaogit1010>
2023-04-25 13:50:05 +0300

    Стилистическое изменение константы URL в rabbitmq.config.ts

--- libs/config/src/rabbitmq.config.ts
+++ libs/config/src/rabbitmq.config.ts
@@ -6,8 +6,7 @@ const HOST = process.env.RABBITMQ_HOST || 'localhost';
 const PORT = process.env.RABBITMQ_PORT || 5672;
 const USER = process.env.RABBITMQ_USER || 'guest';
 const PASSWORD = process.env.RABBITMQ_PASSWORD || 'guest';
-const URL = `amqp://${USER}:${PASSWORD}@` +
-  `${HOST}:${PORT}`;
+const URL = `amqp://${USER}:${PASSWORD}@${HOST}:${PORT}`;
 
 const RMQ_OPTIONS = <RmqOptions>{
   transport: Transport.RMQ,

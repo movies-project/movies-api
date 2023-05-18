748aa7c94a0cb3283c9984c53678f1013c35d8f5
anshexa <109981473+anshexa@users.noreply.github.com>
2023-05-18 22:17:15 +0300

    Добавлен create-user.dto.ts

---
+++ apps/auth/src/user/dto/create-user.dto.ts
@@ -0,0 +1,12 @@
+import { IsNotEmpty, IsEmail } from "class-validator";
+ 
+export class CreateUserDto {
+ 
+    @IsNotEmpty()
+    @IsEmail() 
+    readonly email: string;
+ 
+    @IsNotEmpty()
+    readonly password: string; 
+}
+ 

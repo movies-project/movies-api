695be32c3a492a45b3f625479062a0e7db29d885
anshexa <109981473+anshexa@users.noreply.github.com>
2023-05-20 16:03:58 +0300

    Переименование класса из UserModel в User в user.model.ts

--- apps/auth/src/user/user.model.ts
+++ apps/auth/src/user/user.model.ts
@@ -1,7 +1,7 @@
 import { Model, Table, Column, DataType, Default } from "sequelize-typescript";
 
 @Table({ tableName: 'user'} )
-export class UserModel extends Model {
+export class User extends Model {
   @Column({ type: DataType.BIGINT, primaryKey: true, autoIncrement: true })
   id: number;
 

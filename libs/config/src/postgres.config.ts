4c74cace8f43504c3ddeeca98f0cf9318beaa3eb
anshexa <lilimahiya@gmail.com>
2023-05-21 14:19:05 +0300

    Удаление из postgres.config.ts переменных по умолчанию, добавление example.env, добавление в .gitignore файл .env

--- libs/config/src/postgres.config.ts
+++ libs/config/src/postgres.config.ts
@@ -3,11 +3,11 @@ import { SequelizeModuleOptions } from "@nestjs/sequelize/dist/interfaces/sequel
 export const postgresConfig = {
   SEQUELIZE_OPTIONS: <SequelizeModuleOptions>{
     dialect: 'postgres',
-    host: process.env.POSTGRES_HOST || 'postgres',
-    port: process.env.POSTGRES_PORT || 5432,
-    username: process.env.POSTGRES_USER || 'root',
-    password: process.env.POSTGRES_PASSWORD || 'root',
-    database: process.env.POSTGRES_DB || 'postgres',
+    host: process.env.POSTGRES_HOST,
+    port: Number(process.env.POSTGRES_PORT),
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
     autoLoadModels: true,
     synchronize: true
   },

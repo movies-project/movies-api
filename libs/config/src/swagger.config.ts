50f8d60baa5a6b45a73fbee8c264b2c346e53a08
anshexa <lilimahiya@gmail.com>
2023-05-30 13:25:17 +0300

    Редактирование описания Swagger документации API

--- libs/config/src/swagger.config.ts
+++ libs/config/src/swagger.config.ts
@@ -9,19 +9,25 @@ const docs = {
     PROFILE_API: <DocsConfig>{
         url: 'http://profile:3002/api-docs',
         name: 'Profile API',
-        description: `Profile user data`,
+        description: `### Раздел API для работы с данными профиля пользователя
+        Текущая документация является многостраничной  
+        Выбрать необходимый раздел можно в выпадающем списке в Select a definition`,
         endpoint: 'profile'
     },
     AUTH_API: <DocsConfig>{
         url: 'http://auth:3001/api-docs',
         name: 'Auth API',
-        description: `Authorization and user data API`,
+        description: `### Раздел API для авторизации и получения данных пользователя
+        Текущая документация является многостраничной  
+        Выбрать необходимый раздел можно в выпадающем списке в Select a definition`,
         endpoint: 'auth'
     },
     MOVIE_API: <DocsConfig>{
         url: 'http://movie:3003/api-docs',
         name: 'Movie API',
-        description: `API для работы с фильмами`,
+        description: `### Раздел API для работы с фильмами
+        Текущая документация является многостраничной  
+        Выбрать необходимый раздел можно в выпадающем списке в Select a definition`,
         endpoint: 'movie'
     }
 }

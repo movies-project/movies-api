3b08a0eeb63978323072042bba0fc5ae9bf02d04
AndreyKoshlan <aoaoaogit1010>
2023-06-01 19:23:57 +0300

    Исправление документации Swagger и файлов "Dockerfile"

--- libs/config/src/swagger.config.ts
+++ libs/config/src/swagger.config.ts
@@ -5,29 +5,27 @@ export type DocsConfig = {
     endpoint: string
 }
 
+const SWAGGER_MODULE_DESCRIPTION = '### Раздел API для работы с данными профиля пользователя\n\n'
+    + 'Текущая документация является многостраничной.\n\n'
+    + 'Выбрать необходимый раздел можно в выпадающем списке в «Select a definition».'
+
 const docs = {
-    PROFILE_API: <DocsConfig>{
-        url: 'http://profile:3002/api-docs',
-        name: 'Profile API',
-        description: `### Раздел API для работы с данными профиля пользователя
-        Текущая документация является многостраничной  
-        Выбрать необходимый раздел можно в выпадающем списке в Select a definition`,
-        endpoint: 'profile'
-    },
     AUTH_API: <DocsConfig>{
         url: 'http://auth:3001/api-docs',
         name: 'Auth API',
-        description: `### Раздел API для авторизации и получения данных пользователя
-        Текущая документация является многостраничной  
-        Выбрать необходимый раздел можно в выпадающем списке в Select a definition`,
+        description: SWAGGER_MODULE_DESCRIPTION,
         endpoint: 'auth'
+    },
+    PROFILE_API: <DocsConfig>{
+        url: 'http://profile:3002/api-docs',
+        name: 'Profile API',
+        description: SWAGGER_MODULE_DESCRIPTION,
+        endpoint: 'profile'
     },
     MOVIE_API: <DocsConfig>{
         url: 'http://movie:3003/api-docs',
         name: 'Movie API',
-        description: `### Раздел API для работы с фильмами
-        Текущая документация является многостраничной  
-        Выбрать необходимый раздел можно в выпадающем списке в Select a definition`,
+        description: SWAGGER_MODULE_DESCRIPTION,
         endpoint: 'movie'
     }
 }

55fdce7ea54ed3e334eb8cddf6f3ce7b4148ba66
anshexa <lilimahiya@gmail.com>
2023-06-07 23:52:24 +0300

    Изменение типа для вывода сообщения об ошибке

--- apps/movie/src/genre/common/duplicate-key-bad-request-exception.ts
+++ apps/movie/src/genre/common/duplicate-key-bad-request-exception.ts
@@ -1,7 +1,10 @@
-import { BadRequestException } from "@nestjs/common";
+import {BadRequestException} from "@nestjs/common";
 
 export class DuplicateKeyBadRequestException extends BadRequestException {
-  constructor(key: string, value: string) {
-    super(`Ключ (${key})=(${value}) уже существует`);
-  }
+    constructor(key: string, value: string) {
+        // Заворачиваем в массив, т.к. ошибка 400 может возникать еще и
+        // в случае, если поля не переданы, а там возвращается массив
+        const message: Array<string> = [`Ключ (${key})=(${value}) уже существует`];
+        super(message);
+    }
 }

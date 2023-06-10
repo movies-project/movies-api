76a6c964423efb5c2faf60f5d08d752cef83fd6d
AndreyKoshlan <aoaoaogit1010>
2023-06-10 15:29:14 +0300

    Добавление декораторов валидации в update-movie.dto.ts

--- apps/movie/src/movie/dto/update-movie.dto.ts
+++ apps/movie/src/movie/dto/update-movie.dto.ts
@@ -1,9 +1,19 @@
 import { Movie } from "../models/movie.model";
 import { IntersectionType, PartialType, PickType } from "@nestjs/swagger";
+import { IsString } from "class-validator";
 
 export class UpdateMovieDto extends IntersectionType(
   PickType(Movie, ['name', 'nameEn']),
   PartialType(
     PickType(Movie,['alternativeName'])
   )
-) {}
+) {
+  @IsString()
+  name: string;
+
+  @IsString()
+  nameEn: string;
+
+  @IsString()
+  alternativeName: string;
+}
